20711be93ff45a83cb8f32a5e3b8b389c5663c75
groovin101 <groovin101@gmail.com>
2018-09-05 15:57:56 -0700

    Implements the 'Is this int a Palindrome?' kata. Does not take on challenge of avoiding String usage

--- src/main/java/com/groovin101/kata/beg/KataIsIntPalindrome.java
+++ src/main/java/com/groovin101/kata/beg/KataIsIntPalindrome.java
@@ -1,21 +1,42 @@
 package com.groovin101.kata.beg;
 
+import java.util.Stack;
+
 public class KataIsIntPalindrome {
 
     /**
      * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
-     *
+     * <p>
      * Challenge: implement WITHOUT converting the number to a string
      *
      * @param x
      * @return
      */
     public boolean isPalindrome(int x) {
-        System.out.println(x % 100);
-        System.out.println(x % 10);
-        System.out.println(x % 1);
-        return false;
+        
+        Stack<String> firstHalf = new Stack<>();
+        String intAsString = removeMiddleDigit(Integer.toString(x));
+        int numberOfChars = intAsString.length();
+
+        //push first half of string into a LIFO stack
+        for (int i = 0; i < numberOfChars / 2; i++) {
+            firstHalf.push(intAsString.substring(i, i + 1));
+        }
+
+        //traverse the latter half of string, compare to elements on stack
+        for (int i = numberOfChars / 2; i < numberOfChars; i++) {
+            if (!intAsString.substring(i, i + 1).equals(firstHalf.pop())) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    String removeMiddleDigit(String intAsString) {
+        if ((intAsString.length() % 2) == 0) {
+            return intAsString;
+        }
+        return intAsString.substring(0, intAsString.length() / 2)
+                + intAsString.substring((intAsString.length() / 2) + 1);
+    }
 }
-
--- src/test/java/com/groovin101/kata/beg/KataIsIntPalindromeTest.java
+++ src/test/java/com/groovin101/kata/beg/KataIsIntPalindromeTest.java
@@ -3,15 +3,41 @@ package com.groovin101.kata.beg;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class KataIsIntPalindromeTest {
 
-    //TODO: IMPLEMENT FUNCTIONALITY AND REVISIT
-    @Ignore
+    KataIsIntPalindrome kata = new KataIsIntPalindrome();
+
     @Test
     public void isPalindrome_yes_singleDigit() {
-        KataIsIntPalindrome kata = new KataIsIntPalindrome();
         assertTrue(kata.isPalindrome(7));
+    }
+
+    @Test
+    public void isPalindrome_yes_evenNumberOfDigits() {
+        assertTrue(kata.isPalindrome(7887));
+    }
+
+    @Test
+    public void isPalindrome_no_evenNumberOfDigits() {
+        assertFalse(kata.isPalindrome(78));
+    }
+
+    @Test
+    public void isPalindrome_no_oddNumberOfDigits() {
+        assertFalse(kata.isPalindrome(789));
+    }
+
+    @Test
+    public void removeMiddleDigit_OddNumberOfDigits_3() {
+        assertEquals("13", kata.removeMiddleDigit("123"));
+    }
+
+    @Test
+    public void removeMiddleDigit_OddNumberOfDigits_5() {
+        assertEquals("1245", kata.removeMiddleDigit("12345"));
     }
 }

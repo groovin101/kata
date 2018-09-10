d1b912c0904263184ebcb1655a7c7077e4c1971d
groovin101 <groovin101@gmail.com>
2018-09-10 15:28:38 -0700

    Adds additional tests for fibonacci kata

--- src/main/java/com/groovin101/kata/beg/KataFibonacci.java
+++ src/main/java/com/groovin101/kata/beg/KataFibonacci.java
@@ -1,17 +1,21 @@
 package com.groovin101.kata.beg;
 
+/**
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144...
+ *
+ * Write a function int fib(int n) that returns Fn. For example, if n = 0, then fib() should return 0. If n = 1, then it should return 1. For n > 1, it should return F|n-1 + F|n-2
+ *
+ * Given a number n, print n-th Fibonacci Number
+ */
 public class KataFibonacci {
 
     public int fibonacci(int nthItemInFibSequence) {
-        System.out.println(String.format("nthItemInFibSequence: %d", nthItemInFibSequence));
-        if (nthItemInFibSequence <=0) {
+        if (nthItemInFibSequence == 1) {
             return 0;
         }
-        if (nthItemInFibSequence == 1) {
+        else if (nthItemInFibSequence == 2) {
             return 1;
         }
-        int result = fibonacci(nthItemInFibSequence-1) + fibonacci(nthItemInFibSequence-2);
-        System.out.println(result);
-        return result;
+        return fibonacci(nthItemInFibSequence-1) + fibonacci(nthItemInFibSequence-2);
     }
 }
--- src/test/java/com/groovin101/kata/beg/KataFibonacciTest.java
+++ src/test/java/com/groovin101/kata/beg/KataFibonacciTest.java
@@ -2,11 +2,47 @@ package com.groovin101.kata.beg;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144...
+ *
+ * Given a number n, print n-th Fibonacci Number
+ *
+ *    HINT: Write a function int fib(int n) that returns Fn. For example, if n = 0, then fib() should return 0. If n = 1, then it should return 1. For n > 1, it should return F|n-1 + F|n-2
+ */
 public class KataFibonacciTest {
 
+    KataFibonacci fibonacci = new KataFibonacci();
+
     @Test
-    public void fibonacci() {
-        KataFibonacci fibonacci = new KataFibonacci();
-        fibonacci.fibonacci(8);
+    public void fibonacci_seed_firstPosition() {
+        assertEquals(0, fibonacci.fibonacci(1));
     }
+
+    @Test
+    public void fibonacci_seed_secondPosition() {
+        assertEquals(1, fibonacci.fibonacci(2));
+    }
+
+    @Test
+    public void fibonacci_firstNumThatUsesFormula() {
+        assertEquals(1, fibonacci.fibonacci(3));
+    }
+
+    @Test
+    public void fibonacci_4thNumberInSequence() {
+        assertEquals(2, fibonacci.fibonacci(4));
+    }
+
+    @Test
+    public void fibonacci_5thNumberInSequence() {
+        assertEquals(3, fibonacci.fibonacci(5));
+    }
+
+    @Test
+    public void fibonacci_8thNumberInSequence() {
+        assertEquals(13, fibonacci.fibonacci(8));
+    }
+
 }

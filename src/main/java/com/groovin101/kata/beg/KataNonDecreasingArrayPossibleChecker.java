29a0309bf45d5e3ff698dc259cd3d2cf22e1a1d0
groovin101@gmail.com <groovin101@gmail.com>
2020-11-25 07:02:30 -0800

    Merge remote-tracking branch 'origin/master' into non-decreasing-array
    # Conflicts:
    #	.idea/sonarlint/issuestore/index.pb

--- src/main/java/com/groovin101/kata/beg/KataNonDecreasingArrayPossibleChecker.java
+++ src/main/java/com/groovin101/kata/beg/KataNonDecreasingArrayPossibleChecker.java
@@ -1,5 +1,7 @@
 package com.groovin101.kata.beg;
 
+import java.util.Arrays;
+
 /**
  * Source: https://leetcode.com/problems/non-decreasing-array/
  *
@@ -18,47 +20,17 @@ package com.groovin101.kata.beg;
 public class KataNonDecreasingArrayPossibleChecker {
 
     public boolean checkPossibility(int[] nums) {
-        return checkPossibility_THISWORKS(nums) && checkPossibility_solutionTwo(nums);
+        //int[] nums2 = Arrays.copyOf(nums, nums.length);
+        //return checkPossibility_THISWORKS(nums);
+        return checkPossibility_solutionTwo(nums);
     }
 
     public boolean checkPossibility_solutionTwo(int[] nums) {
 
-        //loop through each number in the set
-        // if the next number (assuming there is one) is less than the current indexed num val, then
-        // we need to adjust *something*
-        // given an order x, y, z, current index x, where x >= y < z, we need to modify either y or
-        // modify z. How to choose which? Let's not get ahead of ourselves. Let's pick one and run
-        // our test through it
-
-        boolean weHaveModifiedTheArray = false;
-
-        //int[] nums = new int[]{5,7,1,8};
-        for (int i = 0; i < nums.length - 1; i++) {
-            //if the next number is bigger than this one, then we are increasing, NOT non-decreasing
-            //that means we need to change either the current indexed number OR the next-indexed number
-            //to ensure that the current one will be <= the next number, but ALSO >= the previous number
-            if (nums[i + 1] < nums[1]) {
-                if (weHaveModifiedTheArray) {
-                    return false;
-                }
-                else if (nums[i] < nums[i+1]) {
-                    nums[i] = nums[i+1];
-                }
-                else {
-                    nums[i + 1] = nums[i];
-                }
-                weHaveModifiedTheArray = true;
-            }
-        }
+        //the trick here is to remember that we may have to adjust different numbers, either the current one
+        //we are examining, or... think about a few scenarios
         return true;
     }
-
-
-
-
-
-
-
 
     public boolean checkPossibility_THISWORKS(int[] nums) {
 

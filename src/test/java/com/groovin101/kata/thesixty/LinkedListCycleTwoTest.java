c0ab975d2a9c94f26a80c09753d4f6e43090ae0d
groovin101 <groovin101@gmail.com>
2021-03-04 12:11:36 -0800

    Clarify comments

--- src/test/java/com/groovin101/kata/thesixty/LinkedListCycleTwoTest.java
+++ src/test/java/com/groovin101/kata/thesixty/LinkedListCycleTwoTest.java
@@ -9,29 +9,29 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+/**
+ * Problem 142: Linked List Cycle - exact node
+ * https://leetcode.com/problems/linked-list-cycle-ii/
+ *
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ *
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+ * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is
+ * connected to. Note that pos is not passed as a parameter.
+ *
+ * Notice that you should not modify the linked list.
+ *
+ * Example 1:
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ */
 public class LinkedListCycleTwoTest {
 
-    /**
-     * Problem 142: Linked List Cycle - exact node
-     * https://leetcode.com/problems/linked-list-cycle-ii/
-     *
-     * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
-     *
-     * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
-     * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is
-     * connected to. Note that pos is not passed as a parameter.
-     *
-     * Notice that you should not modify the linked list.
-     *
-     * Example 1:
-     *
-     * Input: head = [3,2,0,-4], pos = 1
-     * Output: tail connects to node index 1
-     * Explanation: There is a cycle in the linked list, where tail connects to the second node.
-     */
+    // KATA IMPLEMENTATION : tests follow below
     private class LinkedListCycleTwoChecker {
 
-        // KATA IMPLEMENTATION
         public ListNode detectCycle(ListNode head) {
 
             //go through each node by traversing next()
